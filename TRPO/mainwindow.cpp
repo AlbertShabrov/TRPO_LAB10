07789b0a6c2a837d42363eb83445e51ca3b2a7aa
Шабров Альберт <albertshabrov720@gmail.com>
2019-12-24 18:43:50 +0300

    add messagebox

--- TRPO/mainwindow.cpp
+++ TRPO/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QWidget>
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +16,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "Заголовок", "Вы точно хотите выйти?",
+                                                              QMessageBox::Yes | QMessageBox::No);
+    if(reply==QMessageBox::No)
+    {
+        QApplication::quit();
+    }
+    else
+    {
+        qDebug()<<"Кнопка нет была нажата!";
+    }
+}
